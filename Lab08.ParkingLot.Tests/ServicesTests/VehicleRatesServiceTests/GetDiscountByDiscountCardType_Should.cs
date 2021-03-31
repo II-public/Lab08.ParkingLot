98a116a31d48ab9e4a367585cbe986d2786b8adc
Ivaylo Ivanov <iivanov@InnovativeSS.onmicrosoft.com>
2021-03-31 20:27:11 +0300

    Unit tests for vehicle rates service

---
+++ Lab08.ParkingLot.Tests/ServicesTests/VehicleRatesServiceTests/GetDiscountByDiscountCardType_Should.cs
@@ -0,0 +1,30 @@
+﻿using Lab08.ParkingLot.Enums;
+using Lab08.ParkingLot.Service;
+using Lab08.ParkingLot.Service.Interfaces;
+using NUnit.Framework;
+
+namespace Lab08.ParkingLot.Tests.ServicesTests.VehicleRatesServiceTests
+{
+    public class GetDiscountByDiscountCardType_Should
+    {
+        private IVehicleRatesService _vehicleRatesService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _vehicleRatesService = new VehicleRatesService();
+        }
+
+        [Test]
+        [TestCase(10, DiscountCard.Silver)]
+        [TestCase(15, DiscountCard.Gold)]
+        [TestCase(20, DiscountCard.Platinum)]
+        [TestCase(0, DiscountCard.None)]
+        public void ReturnCorrectDiscount_For_CorespondingCardIsPassed(int expectedResult, DiscountCard discountCard)
+        {
+            int actualResult = _vehicleRatesService.GetDiscountByDiscountCardType(discountCard);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ Lab08.ParkingLot.Tests/ServicesTests/VehicleRatesServiceTests/GetRatesByVehicleCategory_Should.cs
@@ -0,0 +1,64 @@
+﻿using Lab08.ParkingLot.Enums;
+using Lab08.ParkingLot.Model;
+using Lab08.ParkingLot.Service;
+using Lab08.ParkingLot.Service.Interfaces;
+using NUnit.Framework;
+
+namespace Lab08.ParkingLot.Tests.ServicesTests.VehicleRatesServiceTests
+{
+    public class GetRatesByVehicleCategory_Should
+    {
+        private IVehicleRatesService _vehicleRatesService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _vehicleRatesService = new VehicleRatesService();
+        }
+
+        [Test]
+        [TestCase(VehicleCategory.A)]
+        public void ReturnCorrectVehicleRate_When_VehicleCatAIsPassed(VehicleCategory vehicleCategory)
+        {
+            var expectedResult = new VehicleRatesModel
+            {
+                DayLightRates = 3,
+                NightlyRates = 2
+            };
+
+            VehicleRatesModel actualResult = _vehicleRatesService.GetRatesByVehicleCategory(vehicleCategory);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        [TestCase(VehicleCategory.B)]
+        public void ReturnCorrectVehicleRate_When_VehicleCatBIsPassed(VehicleCategory vehicleCategory)
+        {
+            var expectedResult = new VehicleRatesModel
+            {
+                DayLightRates = 6,
+                NightlyRates = 4
+            };
+
+            VehicleRatesModel actualResult = _vehicleRatesService.GetRatesByVehicleCategory(vehicleCategory);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        [TestCase(VehicleCategory.C)]
+        public void ReturnCorrectVehicleRate_When_VehicleCatCIsPassed(VehicleCategory vehicleCategory)
+        {
+            var expectedResult = new VehicleRatesModel
+            {
+                DayLightRates = 12,
+                NightlyRates = 8
+            };
+
+            VehicleRatesModel actualResult = _vehicleRatesService.GetRatesByVehicleCategory(vehicleCategory);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
